2a7d365ef63b8184b1fa30caa9b95720bd5d29e5
Gabi <gabrielabguedes@gmail.com>
2020-10-20 16:45:41 -0300

    Hello world java
    Signed-off-by: Gabi <gabrielabguedes@gmail.com>

---
+++ hello_world.java
@@ -0,0 +1,10 @@
+// Hello World em Java
+// Execute pelos comandos:
+// javac hello_world.java
+// java HelloWorld 
+
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World"); 
+    }
+}
